08ed6b93f02febef52e8c9b859b526f3b0d7b605
huangxiao <360817931@qq.com>
2021-06-19 21:54:23 +0800

    a

---
+++ src/Mail.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: hx
+ * Date: 2021/6/19
+ * Time: 21:26
+ */
+
+namespace Huangxiao;
+
+
+class Mail
+{
+    public function send()
+    {
+        return 'mail-send';
+    }
+}
---
+++ src/Upload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: hx
+ * Date: 2021/6/19
+ * Time: 21:26
+ */
+
+namespace Huangxiao;
+
+
+class Upload
+{
+
+    public function file()
+    {
+        return __CLASS__.__METHOD__;
+    }
+
+    public function base64()
+    {
+        return __CLASS__.__METHOD__;
+    }
+}
